be52bf5736aa484c99ebde83884f8a2e4879e572
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-28 10:56:08 -0300

    fix: multiple components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Primeiro from './components/Primeiro';
 import BomDia from './components/BomDia';
+import Multi, {BoaNoite} from './components/Multiplos';
 
 const element = <h1>TESTANDO</h1>
 
-ReactDOM.render(<BomDia nome="Felipe" />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Felipe" />
+        <BoaNoite nome="Bia" />
+    </div>
+    , document.getElementById('root'));
