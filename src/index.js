876b84c4541bc1e6706ca1fbb256c7a18c6ecb2b
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-28 11:17:17 -0300

    fix: class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Primeiro from './components/Primeiro';
-import BomDia from './components/BomDia';
+// import Primeiro from './components/Primeiro';
+//import BomDia from './components/BomDia';
 import Multi, {BoaNoite} from './components/Multiplos';
+import Saudacao from './components/Saudacao';
 
 const element = <h1>TESTANDO</h1>
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Felipe" />
-        <BoaNoite nome="Bia" />
+        <Saudacao tipo="Bom dia" nome="Felipe" />
     </div>
     , document.getElementById('root'));
